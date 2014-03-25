53cd0cba9ce077a4620d61d8b137eb93835f1316
Ashley Moni <y30099923@gmail.com>
2014-03-25 00:36:37 -0400

    Qualifying all the imports, removing the borders. Time to get ready to rumble!

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,22 @@
-import XMonad
-import XMonad.Config.Gnome
+-------------------------------------------------- Base
+import XMonad.Main (xmonad)
+import XMonad.Core (terminal, modMask, workspaces, layoutHook)
 
-main = xmonad gnomeConfig
-       { modMask = mod4Mask
-       , focusedBorderColor = "#ffaf00" }
+import XMonad.Config (defaultConfig)
+
+import Graphics.X11.Types (mod4Mask)
+-------------------------------------------------- Utilities
+import XMonad.Layout.NoBorders (noBorders)
+
+main = xmonad defaultConfig
+       { 
+         terminal = "sakura",
+         modMask = mod4Mask,
+
+         workspaces = ["web", "irc", "code", "shell"] ++ map show [5..9],
+
+         layoutHook = noBorders $ layoutHook defaultConfig
+
+         -- normalBorderColor  = "#dddddd",
+         -- focusedBorderColor = "#ffaf00"
+       }
