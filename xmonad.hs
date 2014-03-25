ad486da0fe0e07123085ab91930038f5cceab623
Ashley Moni <y30099923@gmail.com>
2014-03-25 14:31:11 -0400

    Update for trill

--- xmonad.hs
+++ xmonad.hs
@@ -1,22 +1,28 @@
+-- Imports:
 -------------------------------------------------- Base
 import XMonad.Main (xmonad)
-import XMonad.Core (terminal, modMask, workspaces, layoutHook)
+import XMonad.Core (terminal, modMask, workspaces, layoutHook,
+                    normalBorderColor,focusedBorderColor)
 
 import XMonad.Config (defaultConfig)
 
 import Graphics.X11.Types (mod4Mask)
 -------------------------------------------------- Utilities
+import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Layout.NoBorders (noBorders)
 
+-- Config:
+main :: IO ()
 main = xmonad defaultConfig
-       { 
-         terminal = "sakura",
-         modMask = mod4Mask,
+  { terminal   = "sakura",
+    modMask    = mod4Mask,
 
-         workspaces = ["web", "irc", "code", "shell"] ++ map show [5..9],
+    workspaces = ["web", "irc", "code", "shell"]
+                 ++ map show ([5..9] :: [Int]),
 
-         layoutHook = noBorders $ layoutHook defaultConfig
+    normalBorderColor  = "#dddddd",
+    focusedBorderColor = "#ffaf00",
 
-         -- normalBorderColor  = "#dddddd",
-         -- focusedBorderColor = "#ffaf00"
-       }
+  -- Remove borders, don't overlap with dzen/xmobar.
+    layoutHook = avoidStruts . noBorders
+                 $ layoutHook defaultConfig }
