80bb7b52dc9d31461ac5dc5eb3c6d96f74f6142d
Ashley Moni <y30099923@gmail.com>
2014-03-27 13:14:20 -0400

    Updating xmonad.hs with my arrow key movement

--- xmonad.hs
+++ xmonad.hs
@@ -1,27 +1,36 @@
 -- Imports:
-import XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
 -------------------------------------------------- Base
 import XMonad.Main (xmonad)
-import XMonad.Core (terminal, modMask, workspaces,
-                    layoutHook, logHook,
-                    normalBorderColor,focusedBorderColor)
+import XMonad.Core (WindowSet, WorkspaceId,
+  terminal, modMask, workspaces,
+  layoutHook, logHook,
+  normalBorderColor,focusedBorderColor)
 
 import XMonad.Config (defaultConfig)
+import XMonad.Operations (windows)
 
 import Graphics.X11.Types (mod4Mask)
--------------------------------------------------- Utilities
-import XMonad.Hooks.ManageDocks (avoidStruts)
+-------------------------------------------------- Workspaces
+import XMonad.StackSet (view, currentTag, focusUp, focusDown)
+-------------------------------------------------- Windows
+import XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
 import XMonad.Layout.NoBorders (noBorders)
+-------------------------------------------------- Xmobar / Dzen
+import XMonad.Hooks.ManageDocks (avoidStruts)
+-------------------------------------------------- Utilities
+import XMonad.Util.EZConfig (additionalKeysP)
 
 -- Config:
 
+workspaceTags :: [String]
+workspaceTags = ["web", "irc", "code", "shell"] ++ map show ([5..9] :: [Int])
+
 main :: IO ()
-main = xmonad defaultConfig
+main = xmonad $ defaultConfig
   { terminal   = "sakura",
     modMask    = mod4Mask,
 
-    workspaces = ["web", "irc", "code", "shell"]
-                 ++ map show ([5..9] :: [Int]),
+    workspaces = workspaceTags,
 
     normalBorderColor  = "#dddddd",
     focusedBorderColor = "#ffaf00",
@@ -32,3 +41,34 @@ main = xmonad defaultConfig
   -- Remove borders, don't overlap with dzen/xmobar.
     layoutHook = avoidStruts . noBorders
                  $ layoutHook defaultConfig }
+  
+  `additionalKeysP` [ ("M-<Left>",  windows focusLeft),
+                      ("M-<Right>", windows focusRight),
+                      ("M-<Up>",    windows focusUp),
+                      ("M-<Down>",  windows focusDown) ]
+
+-- focusLeft moves you to the workspace immediately left, focusRight
+-- moves you to the workspace immediately right.
+
+focusLeft :: WindowSet -> WindowSet
+focusLeft stack = maybeMove $ lookup (currentTag stack) leftMap
+  where maybeMove Nothing  = stack
+        maybeMove (Just i) = view i stack
+
+focusRight :: WindowSet -> WindowSet
+focusRight stack = maybeMove $ lookup (currentTag stack) rightMap
+  where maybeMove Nothing  = stack
+        maybeMove (Just i) = view i stack
+
+-- rightMap and leftMap map every workspace to the workspace on its
+-- immediate right and left respectively.
+
+rightMap :: [(WorkspaceId, WorkspaceId)]
+rightMap = case workspaceTags of
+             []     -> []
+             (_:tl) -> zip workspaceTags tl
+
+leftMap :: [(WorkspaceId, WorkspaceId)]
+leftMap = case workspaceTags of
+             []     -> []
+             (_:tl) -> zip tl workspaceTags
