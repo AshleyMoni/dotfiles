0c15404aee7d41427194cca6fe0e2feb2de1f506
Ashley Moni <y30099923@gmail.com>
2014-03-28 12:50:49 -0400

    I've been reimplementing CycleWS functionality, so I've refactored to use that.

--- xmonad.hs
+++ xmonad.hs
@@ -1,17 +1,17 @@
 -- Imports:
 -------------------------------------------------- Base
 import XMonad.Main (xmonad)
-import XMonad.Core (WindowSet, WorkspaceId,
-  terminal, modMask, workspaces,
-  layoutHook, logHook, manageHook,
-  normalBorderColor,focusedBorderColor)
+import XMonad.Core (terminal, modMask, workspaces,       
+                    layoutHook, logHook, manageHook,     
+                    normalBorderColor,focusedBorderColor)
 
 import XMonad.Config (defaultConfig)
 import XMonad.Operations (windows)
 
 import Graphics.X11.Types (mod4Mask)
 -------------------------------------------------- Workspaces
-import XMonad.StackSet (view, currentTag, focusUp, focusDown)
+import XMonad.StackSet (focusUp, focusDown)
+import XMonad.Actions.CycleWS (nextWS, prevWS)
 -------------------------------------------------- Windows
 import XMonad.Hooks.FadeInactive (fadeInactiveLogHook)
 import XMonad.Layout.NoBorders (noBorders)
@@ -47,33 +47,7 @@ main = xmonad $ defaultConfig
     layoutHook = avoidStruts . noBorders
                  $ layoutHook defaultConfig }
   
-  `additionalKeysP` [ ("M-<Left>",  windows focusLeft),
-                      ("M-<Right>", windows focusRight),
+  `additionalKeysP` [ ("M-<Left>",  prevWS),
+                      ("M-<Right>", nextWS),
                       ("M-<Up>",    windows focusUp),
                       ("M-<Down>",  windows focusDown) ]
-
--- focusLeft moves you to the workspace immediately left, focusRight
--- moves you to the workspace immediately right.
-
-focusLeft :: WindowSet -> WindowSet
-focusLeft stack = maybeMove $ lookup (currentTag stack) leftMap
-  where maybeMove Nothing  = stack
-        maybeMove (Just i) = view i stack
-
-focusRight :: WindowSet -> WindowSet
-focusRight stack = maybeMove $ lookup (currentTag stack) rightMap
-  where maybeMove Nothing  = stack
-        maybeMove (Just i) = view i stack
-
--- rightMap and leftMap map every workspace to the workspace on its
--- immediate right and left respectively.
-
-rightMap :: [(WorkspaceId, WorkspaceId)]
-rightMap = case workspaceTags of
-             []     -> []
-             (_:tl) -> zip workspaceTags tl
-
-leftMap :: [(WorkspaceId, WorkspaceId)]
-leftMap = case workspaceTags of
-             []     -> []
-             (_:tl) -> zip tl workspaceTags
