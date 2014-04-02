a32aa7afaa72c046ff52ac02cd6134d132bfead5
Ashley Moni <y30099923@gmail.com>
2014-04-02 12:29:09 -0400

    Cleaning up my xmonad.hs!

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,7 +13,7 @@ import XMonad.Hooks.EwmhDesktops (ewmh)
 
 import Graphics.X11.Types (mod4Mask, xK_Tab, xK_grave,
                            xK_Super_L, xK_Super_R,
-                           xK_Alt_L,   xK_Alt_R)
+                           xK_Alt_L, xK_Alt_R)
 -------------------------------------------------- Workspaces
 import XMonad.StackSet (focusUp, focusDown, swapUp, swapDown,
                         greedyView, shift)
@@ -28,39 +28,70 @@ import XMonad.Layout.Spacing (smartSpacing)
 -------------------------------------------------- Dzen
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, dzenPP,
-  ppOutput, ppTitle, ppCurrent, ppVisible, ppHidden, ppUrgent,
-  ppHiddenNoWindows, ppWsSep, ppSep, ppLayout, ppOrder,
+  ppCurrent, ppHidden, ppUrgent, ppHiddenNoWindows,
+  ppWsSep, ppSep, ppLayout, ppOrder, ppOutput, ppTitle,
   dzenColor, wrap, shorten)
 
 import XMonad.Hooks.ManageDocks (avoidStruts)
 -------------------------------------------------- Utilities
 import XMonad.Util.EZConfig (additionalKeysP)
 import Data.Monoid ((<>))
+
 import System.IO (hPutStrLn)
+import System.Environment (getEnv)
 
--- Config:
+---- Config variables:
 
-myXmonadBar :: String
-myXmonadBar = "dzen2 -x '0' -y '0' -h '13' -w '866' -ta 'l' -fg '"
-              ++ foreground ++ "' -bg '" ++ background ++ "' -fn " ++ font
-myStatusBar :: String
-myStatusBar = "conky -qc /home/kron/.xmonad/.conky_dzen | dzen2 -x '866' -y '0' -w '500' -h '13' -ta 'r' -fg '"
-              ++ foreground ++ "' -bg '" ++ background ++ "' -fn " ++ font
+font = "-*-terminus-medium-*-normal-*-12-*-*-*-*-*-*-*"
 
-myWorkspaces :: [String]
--- myWorkspaces = clickable ["^i(/home/kron/.xmonad/dzen2/fox.xbm)", -- main / web
---                           "^i(/home/kron/.xmonad/dzen2/mail.xbm)", -- chat / comm
---                           "^i(/home/kron/.xmonad/dzen2/code.xbm)", -- code
+foreground = orange
+background = black
+
+orange = "#ffa500"
+black  = "#000000"
+
+-- Files from dzen directory to surround dzen workspace entries with:
+workspaceBraces = ("side_l.xbm", "side_r.xbm")
+
+---- Bars, Workspaces
+
+-- myWorkspaces = clickable ["^i(/home/kron/.xmonad/dzen2/fox.xbm)",        -- main / web
+--                           "^i(/home/kron/.xmonad/dzen2/mail.xbm)",       -- chat / comm
+--                           "^i(/home/kron/.xmonad/dzen2/code.xbm)",       -- code
 --                           "^i(/home/kron/.xmonad/dzen2/arch_10x10.xbm)", -- shell
 myWorkspaces = clickable ["main", "chat", "code", "shell",
                           "v", "vi", "vii", "viii", "ix", "x"]
   where clickable tags = [ "^ca(1,xdotool key super+" ++ show i ++ ")" ++ ws ++ "^ca()" |
                            (i,ws) <- zip ([1..9] ++ [0] :: [Int]) tags ]
 
-main :: IO ()
+---- Locations
+-- $HOME will be automatically prepended to all these locations.
+
+conkyPath = configDir ++ "/.conky_dzen" -- Location of conky file for right bar
+dzenDir   = configDir ++ "/dzen2"       -- Location of dzen files for the left bar
+
+configDir = "/.xmonad" -- I place my dzen and conky files in my .xmonad folder
+
+---- XMonad
+
 main = do
-  _           <- spawnPipe myStatusBar
-  dzenLeftBar <- spawnPipe myXmonadBar
+  home <- getEnv "HOME"
+  let dzen  = home ++ dzenDir
+      conky = home ++ conkyPath
+
+      wrappedIn = workspaceFormat dzen
+
+  -- dzenRightBar is a wrapped conky that we don't handle.
+  spawn $ "conky -qc " ++ conky ++ " | dzen2 -x '866' -y '0' -w '500' -h '13' -ta 'r'"
+          ++ " -fg '" ++ foreground ++ "'"
+          ++ " -bg '" ++ background ++ "'"
+          ++ " -fn "  ++ font
+
+  dzenLeftBar <- spawnPipe $
+    "dzen2 -x '0' -y '0' -h '13' -w '866' -ta 'l'"
+    ++ " -fg '" ++ foreground ++ "'"
+    ++ " -bg '" ++ background ++ "'"
+    ++ " -fn "  ++ font
 
   xmonad $ ewmh defaultConfig {
     terminal   = "sakura",
@@ -68,33 +99,29 @@ main = do
 
     workspaces = myWorkspaces,
 
-    normalBorderColor  = "#000000",
-    focusedBorderColor = "#ffa500",
+    focusedBorderColor = foreground,
+    normalBorderColor  = background,
 
   -- Fade out inactive windows, set up xdzen
     logHook = fadeInactiveLogHook 0.9 >>
               dynamicLogWithPP (dzenPP {
-                  ppCurrent = wrap "^fg(#ffa500)^i(/home/kron/.xmonad/dzen2/side_l.xbm)^fg(#000000)^bg(#ffa500)"
-                                   "^bg()^fg(#ffa500)^i(/home/kron/.xmonad/dzen2/side_r.xbm)^fg()"
-                , ppVisible = wrap "^fg(#262626)^i(/home/kron/.xmonad/dzen2/side_l.xbm)^fg(#dcdcdc)^bg(#1a1a1a)"
-                                   "^bg()^fg(#1a1a1a)^i(/home/kron/.xmonad/dzen2/side_r.xbm)^fg()"
-                , ppHidden = wrap "^fg(#1a1a1a)^i(/home/kron/.xmonad/dzen2/side_l.xbm)^fg(#dcdcdc)^bg(#1a1a1a)"
-                                  "^bg()^fg(#1a1a1a)^i(/home/kron/.xmonad/dzen2/side_r.xbm)^fg()"
-                , ppHiddenNoWindows = wrap "^fg(#1a1a1a)^i(/home/kron/.xmonad/dzen2/side_l.xbm)^fg(#404040)^bg(#1a1a1a)"
-                                           "^bg()^fg(#1a1a1a)^i(/home/kron/.xmonad/dzen2/side_r.xbm)^fg()"
+                  ppCurrent         = black     `wrappedIn` orange
+                , ppHidden          = "#dcdcdc" `wrappedIn` "#1a1a1a"
+                , ppHiddenNoWindows = "#404040" `wrappedIn` "#1a1a1a"
                 , ppUrgent = wrap "^fg(#f92672)^i(/home/kron/.xmonad/dzen2/side_l.xbm)^fg(#dcdcdc)^bg(#f92672)^i(/home/kron/.xmonad/dzen2/notice.xbm) "
                                   "^bg()^fg(#f92672)^i(/home/kron/.xmonad/dzen2/side_r.xbm)"
                 , ppWsSep  = ""
                 , ppSep    = "  "
-                , ppLayout = wrap "^ca(1,xdotool key super+space)" "^ca()" . dzenColor orange background .
-                               (\x -> case drop 2 $ words x of
-                                        ["Tall"]           -> "^i(/home/kron/.xmonad/dzen2/tall.xbm)"
-                                        ["Mirror", "Tall"] -> "^i(/home/kron/.xmonad/dzen2/mtall.xbm)"
-                                        ["Full"]           -> "^i(/home/kron/.xmonad/dzen2/full.xbm)"
-                                        _                  -> "^i(/home/kron/.xmonad/dzen2/grid.xbm)")
---              , ppTitle  =  wrap "^ca(1,xdotool key super+shift+x)^fg(#D23D3D)^fn(fkp)x ^fn()" "^ca()" . dzenColor foreground background . shorten 40 . pad
-                , ppTitle  =  dzenColor orange background . shorten 70
+                , ppLayout = dzenColor foreground background .
+                             wrap ("^ca(1,xdotool key super+space)^i(" ++ dzen ++ "/") ")^ca()" .
+                             (\x -> case drop 2 $ words x of
+                                      ["Tall"]           -> "tall.xbm"
+                                      ["Mirror", "Tall"] -> "mtall.xbm"
+                                      ["Full"]           -> "full.xbm"
+                                      _                  -> "grid.xbm")
+                , ppTitle  =  wrap "^ca(2,xdotool key super+shift+c)" "^ca()" . dzenColor foreground background . shorten 80
                 , ppOrder  =  id
+                -- , ppOutput =  wrap "^ca(2,xdotool key super+left)" "^ca()" >>= hPutStrLn dzenLeftBar
                 , ppOutput =  hPutStrLn dzenLeftBar
                }),
 
@@ -123,15 +150,17 @@ main = do
 
                         ("M-q", spawn "xmonad --recompile && killall conky && killall dzen2 && xmonad --restart")]
 
+---- Helpers
 
-font :: String
-font = "-*-terminus-medium-*-normal-*-12-*-*-*-*-*-*-*"
+-- Generate dzen format for workspace entries
+workspaceFormat :: String -> String -> String -> String -> String
+workspaceFormat dzen fg bg = wrap left right
+  where left  = "^fg("++ bg ++ ")" ++
+                "^i(" ++ dzen ++ "/" ++ leftBrace ++ ")" ++
+                "^fg(" ++ fg ++ ")^bg(" ++ bg ++ ")"
 
-background :: String
-background = "#000000"
+        right = "^bg()^fg(" ++ bg ++ ")" ++
+                "^i(" ++ dzen ++ "/" ++ rightBrace ++ ")" ++
+                "^fg()"
 
-foreground :: String
-foreground = orange
-
-orange :: String
-orange = "#ffa500"
+        (leftBrace, rightBrace) = workspaceBraces
